16c2d82c55c0683415c152cc9735a79e130d18d2
Thiago Brasil <trbrasil.ce@gmail.com>
2016-10-08 18:18:09 -0300

    ControllerLogin verificando campos user/pass

---
+++ BoraComer/src/main/java/br/com/alive/boracomer/controller/controller.java
@@ -0,0 +1,16 @@
+package br.com.alive.boracomer.controller;
+
+import br.com.alive.boracomer.entity.Usuario;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.transaction.Transactional;
+
+@Named
+@RequestScoped
+public class controller {
+    
+    
+}
