472be9ef280082337eb5b554e9023e800a277fb7
Valeria O. Brito <valeriiabrito@gmail.com>
2016-10-10 21:23:35 -0300

    commit

--- BoraComer/src/main/java/br/com/alive/boracomer/controller/ControllerHome.java
+++ BoraComer/src/main/java/br/com/alive/boracomer/controller/ControllerHome.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.alive.boracomer.controller;
 
 import javax.enterprise.context.RequestScoped;
@@ -13,11 +8,11 @@ import javax.inject.Named;
 public class ControllerHome {
     
     public String logout(){
-        return "faces/index?faces-redirect=true";
+        return "index?faces-redirect=true";
     }
     
     public String novoEvento(){
-        return "/novoEvento?faces-redirect=true";
+        return "novoEvento?faces-redirect=true";
     }
     
 }
