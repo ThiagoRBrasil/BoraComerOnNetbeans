c1eb5a37ac2a374a0c2b38fbece47f19f1271923
Ivina Santos <ivina.p.santos@gmail.com>
2016-10-17 22:03:07 -0200

    commit

--- BoraComer/src/main/java/br/com/alive/boracomer/controller/ControllerHome.java
+++ BoraComer/src/main/java/br/com/alive/boracomer/controller/ControllerHome.java
@@ -8,19 +8,11 @@ import javax.inject.Named;
 public class ControllerHome {
     
     public String logout(){
-<<<<<<< HEAD
-        return "index?redirect=true";
-    }
-    
-    public String novoEvento(){
-        return "index?faces-redirect=true";
-=======
         return "index?faces-redirect=true";
     }
     
     public String novoEvento(){
         return "novoEvento?faces-redirect=true";
->>>>>>> 90d67a0c585e7cbe5677e2f9f829c23557d23556
     }
-    
+     
 }
