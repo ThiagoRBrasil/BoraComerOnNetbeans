5bdb20590092391f7bf2b5369b635b5fafebdc4b
Thiago Brasil <trbrasil.ce@gmail.com>
2016-10-19 18:41:56 -0300

    commit

--- BoraComer/src/main/java/br/com/alive/boracomer/controller/ControllerLogin.java
+++ BoraComer/src/main/java/br/com/alive/boracomer/controller/ControllerLogin.java
@@ -1,12 +1,10 @@
 package br.com.alive.boracomer.controller;
 
-import br.com.alive.boracomer.dao.UsuarioDAO;
 import br.com.alive.boracomer.entity.Usuario;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
-import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -19,27 +17,15 @@ public class ControllerLogin implements Serializable {
     private String user;
     private String pass;
 
-    private Usuario usuario;
-    private List<Usuario> usuarios;
-
     @PostConstruct
     public void reset() {
         this.user = null;
         this.pass = null;
-        usuario = new Usuario();
-        usuarios = new ArrayList<>();
     }
 
     public String loginUsuario() {
-//        System.out.println("metodo invocadooooooooooooooooooo");
-        usuarios = (List<Usuario>) UsuarioDAO.getInstance();
-        if (usuarios != null) {
-            for (int i = 0; i < usuarios.size(); i++) {
-                if (this.user.equals(usuarios.get(i).getEmail())
-                        && this.pass.equals(usuarios.get(i).getPass())) {
-                    return "home?faces-redirect=true";
-                }
-            }
+        if (this.user.equals("admin") && this.pass.equals(pass)) {
+            return "home?faces-redirect=true";
         }
         addMessage("Usuario/Senha incorreto(s)");
         return null;
